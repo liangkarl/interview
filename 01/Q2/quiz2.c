eb9110a41bcf465949e5520100f6be383629bdf9
Karl Liang <liang.karl@outlook.com>
2023-09-17 13:19:02 -0600

    01-Q2: Add the implementation of quiz 2
    [Description]
    a) Find function f(Ai) using these sets of points in the spreadsheet.
    b) What would be the output for the following inputs?
    	* f(30001) = ?
    	* f(55555) = ?
    	* f(77788) = ?
    c) What is the upper limit or maximum range of this function before
       there will be collisions or overflow?
    d) Please share any code that you write in the process of solving
       the above problems.
    
    a) f(Ai) is a function that convert decimal numbers into 62-based
       numbers where the digits and alphabetic characters are rearranged,
       which means the next number of 5 may not be 6.
       The mapping table is as below,
    	0=C,    1=7,    2=x,    3=i,    4=c,    5=P,    6=M,    7=G,
    	8=v,    9=z,    10=A,   11=Z,   12=y,   13=T,   14=N,   15=o,
    	16=d,   17=m,   18=w,   19=n,   20=V,   21=5,   22=D,   23=3,
    	24=B,   25=6,   26=H,   27=0,   28=O,   29=u,   30=p,   31=8,
    	32=E,   33=2,   34=1,   35=W,   36=9,   37=s,   38=q,   39=L,
    	40=Q,   41=X,   42=4,   43=Y,   44=j,   45=S,   46=e,   47=U,
    	48=h,   49=I,   50=R,   51=J,   52=g,   53=a,   54=f,   55=F,
    	56=t,   57=r,   58=K,   59=l,   60=b,   61=k,
    b) f(30001) = GIF
       f(55555) = NOi
       f(77788) = VNQ
    c) There is no upper limit or maximum range in this function.
    d) Please reference the source code
    
    [Test]
    Test set 1:
    All the numbers in the original spreadsheet.
    
    Test set 2:
    30001
    55555
    77788

---
+++ 01/Q2/quiz2.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define N	62
+#define OUTPUT	"output.txt"
+#define INPUT	"input.txt"
+#define DATA	"data.txt"
+
+int build_table(FILE *fdata, char *table)
+{
+	int x, i;
+	char y[4];
+
+	/* Rebuild the table from decimal number to 62-based number. */
+	while (fscanf(fdata, "%d\t%s\n", &x, y) == 2) {
+		table[x % N] = y[2];
+		table[x / N % N] = y[1];
+		table[x / N / N % N] = y[0];
+	}
+
+	/*
+	 * In the spreadsheet, there is no sample for y=f(21), but it's
+	 * easy to find out that f(21)=5
+	 */
+	table[21] = '5';
+
+#ifdef DEBUG
+	printf("Mapping Table:\n");
+	for (i = 0; i < N; i++) {
+		printf("%d=%c,\t", i, table[i]);
+		if (i % 8 == 7)
+			puts("");
+	}
+	puts("");
+#endif
+
+	return 0;
+}
+
+void f(char *table, int x, char *y)
+{
+	static int i;
+
+	if (x == 0)
+		return;
+
+	i = 0;
+	f(table, x / N, y);
+	y[i++] = table[x % N];
+}
+
+int main(int argc, char **argv)
+{
+	FILE *fdata, *fin, *fout;
+	char table[64] = {0};
+	char y[16] = {0};
+	int x;
+
+	fdata = fopen(DATA, "r");
+	if (!fdata) {
+		fprintf(stderr, "Error opening data file, %s\n", DATA);
+		goto err_data;
+	}
+
+	fin = fopen(INPUT, "r");
+	if (!fin) {
+		fprintf(stderr, "Error opening output file, %s\n", INPUT);
+		goto err_in;
+	}
+
+	fout = fopen(OUTPUT, "w");
+	if (!fout) {
+		fprintf(stderr, "Error opening output file, %s\n", OUTPUT);
+		goto err_out;
+	}
+
+	build_table(fdata, table);
+
+	while (fscanf(fin, "%d", &x) == 1) {
+		f(table, x, y);
+		fprintf(fout, "%s\n", y);
+	};
+
+	fclose(fin);
+	fclose(fout);
+	fclose(fdata);
+
+	return 0;
+
+err_out:
+	fclose(fin);
+err_in:
+	fclose(fdata);
+err_data:
+
+	return EINVAL;
+}
